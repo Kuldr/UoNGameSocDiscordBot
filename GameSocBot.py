d46e2caf052808cd41d28f0042894ec19cd5978c
Benjamin Charlton <kuldrgames@gmail.com>
2019-08-20 23:14:55 +0100

    Add the ability to remove roles

--- GameSocBot.py
+++ GameSocBot.py
@@ -49,6 +49,13 @@ async def add_role_from_id(member, role_id):
         if i.id == role_id:
             await member.add_roles(i)
 
+# Helper function that removes the specified role (w/ role_id) to the user
+async def remove_role_from_id(member, role_id):
+    roles = client.get_guild(GAMESOC_GUILD_ID).roles
+    for i in roles:
+        if i.id == role_id:
+            await member.remove_roles(i)
+
 # When the client is set up and conneted it will print to the system running
 #   the bot that it has connected
 @client.event
@@ -113,6 +120,55 @@ async def on_raw_reaction_add(payload):
                 message = await client.get_channel(ROLEALLOCATION_CHANNEL_ID).fetch_message(MESSAGE_TO_REACT_TO_ID)
                 await message.remove_reaction(payload.emoji, member)
                 print("Reaction not in list; Removing reaction")
+
+# If a user removes react to the correct message, removes the role from the user
+@client.event
+async def on_raw_reaction_remove(payload):
+        # Need to make bot not react to its own reactions
+        if client.user.id != payload.user_id:
+            # Check that the reaction was remove to the message that I want to checks
+            if payload.message_id == MESSAGE_TO_REACT_TO_ID:
+                reactionID = payload.emoji.id
+                member = client.get_guild(payload.guild_id).get_member(payload.user_id)
+                print("removed reacted to correct message")
+                # Check to see which reaction was added
+                # When a recognised reaction is added give the user the relevant role
+                if reactionID == LEAGUE_EMOJI_ID:
+                    print("Reacted League")
+                    await remove_role_from_id(member, LEAGUE_ROLE_ID)
+                elif reactionID == DOTA_EMOJI_ID:
+                    print("Reacted DotA")
+                    await remove_role_from_id(member, DOTA_ROLE_ID)
+                elif reactionID == OVERWATCH_EMOJI_ID:
+                    print("Reacted OW")
+                    await remove_role_from_id(member, OVERWATCH_ROLE_ID)
+                elif reactionID == CSGO_EMOJI_ID:
+                    print("Reacted CSGO")
+                    await remove_role_from_id(member, CSGO_ROLE_ID)
+                elif reactionID == ROCKET_LEAGUE_EMOJI_ID:
+                    print("Reacted Rocket League")
+                    await remove_role_from_id(member, ROCKET_LEAGUE_ROLE_ID)
+                elif reactionID == HEARTHSTONE_EMOJI_ID:
+                    print("Reacted Hearthstone")
+                    await remove_role_from_id(member, HEARTHSTONE_ROLE_ID)
+                elif reactionID == RAINBOW_SIX_EMOJI_ID:
+                    print("Reacted Rainbow Six Siege")
+                    await remove_role_from_id(member, RAINBOW_SIX_ROLE_ID)
+                elif reactionID == PUBG_EMOJI_ID:
+                    print("Reacted PUBG")
+                    await remove_role_from_id(member, PUBG_ROLE_ID)
+                elif reactionID == FG_EMOJI_ID:
+                    print("Reacted Fighting Game")
+                    await remove_role_from_id(member, FG_ROLE_ID)
+                elif reactionID == WOW_EMOJI_ID:
+                    print("Reacted PUBG")
+                    await remove_role_from_id(member, WOW_ROLE_ID)
+                elif reactionID == FORTNITE_EMOJI_ID:
+                    print("Reacted Fortnite")
+                    await remove_role_from_id(member, FORTNITE_ROLE_ID)
+                elif reactionID == DESTINY_EMEOJI_ID:
+                    print("Reacted Destiny")
+                    await remove_role_from_id(member, DESTINY_ROLE_ID)
 
 #Simple command to check the server size
 @client.event
