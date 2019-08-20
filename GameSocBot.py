b1a75752354fa85c41d0c9a7a5e39917657a3086
Benjamin Charlton <kuldrgames@gmail.com>
2019-08-20 22:27:35 +0100

    Rearranged IDs and reaction if statement to be consitent

--- GameSocBot.py
+++ GameSocBot.py
@@ -7,33 +7,27 @@ from login import TOKEN
 MESSAGE_TO_REACT_TO_ID = 490888395242078218
 GAMESOC_GUILD_ID = 114348469241643011
 # Emoji IDs
-OVERWATCH_EMOJI_ID = 490883226454327316
 LEAGUE_EMOJI_ID = 490883189896642560
-FORTNITE_EMOJI_ID = 490883173551570945
 DOTA_EMOJI_ID = 490883165884514334
-CSGO_EMOJI_ID = 490883131939749918
-WOW_EMOJI_ID = 490883328959053824
+OVERWATCH_EMOJI_ID = 490883226454327316
+CSGO_EMOJI_ID = 612055189121138688
 ROCKET_LEAGUE_EMOJI_ID = 490883269605195788
 HEARTHSTONE_EMOJI_ID = 490883182397227009
-DESTINY_EMEOJI_ID = 490883146540253194
 RAINBOW_SIX_EMOJI_ID = 490883260193177640
 PUBG_EMOJI_ID = 490883251142000640
-MTG_EMOJI_ID = 490883199044681742
-WARFRAME_EMOJI_ID = 490883287556816896
+FORTNITE_EMOJI_ID = 490883173551570945
+DESTINY_EMEOJI_ID = 612077075356057628
 # Role IDs
-OVERWATCH_ROLE_ID = 360239768317067264
 LEAGUE_ROLE_ID = 360239401839624214
-FORTNITE_ROLE_ID = 420236263233159169
 DOTA_ROLE_ID = 360464804865114115
+OVERWATCH_ROLE_ID = 360239768317067264
 CSGO_ROLE_ID = 360239648162709505
-WOW_ROLE_ID = 370643554655141888
 ROCKET_LEAGUE_ROLE_ID = 360465836852641792
 HEARTHSTONE_ROLE_ID = 360467250634817536
-DESTINY_ROLE_ID = 372823107905257492
 RAINBOW_SIX_ROLE_ID = 420351992133582861
 PUBG_ROLE_ID = 360238836623605760
-MTG_ROLE_ID = 474253927278182410
-WARFRAME_ROLE_ID = 488835550258659348
+FORTNITE_ROLE_ID = 420236263233159169
+DESTINY_ROLE_ID = 372823107905257492
 # MISC
 GAMESOCBOT_ROLE_ID = 490890046405345312
 
@@ -72,45 +66,36 @@ async def on_raw_reaction_add(payload):
             print("reacted to correct message")
             # Check to see which reaction was added
             # When a recognised reaction is added give the user the relevant role
-            if reactionID == OVERWATCH_EMOJI_ID:
-                print("Reacted OW")
-                await add_role_from_id(member, OVERWATCH_ROLE_ID)
-            elif reactionID == LEAGUE_EMOJI_ID:
+            if reactionID == LEAGUE_EMOJI_ID:
                 print("Reacted League")
                 await add_role_from_id(member, LEAGUE_ROLE_ID)
-            elif reactionID == FORTNITE_EMOJI_ID:
-                print("Reacted Fortnite")
-                await add_role_from_id(member, FORTNITE_ROLE_ID)
             elif reactionID == DOTA_EMOJI_ID:
                 print("Reacted DotA")
                 await add_role_from_id(member, DOTA_ROLE_ID)
+            elif reactionID == OVERWATCH_EMOJI_ID:
+                print("Reacted OW")
+                await add_role_from_id(member, OVERWATCH_ROLE_ID)
             elif reactionID == CSGO_EMOJI_ID:
                 print("Reacted CSGO")
                 await add_role_from_id(member, CSGO_ROLE_ID)
-            elif reactionID == WOW_EMOJI_ID:
-                print("Reacted WoW")
-                await add_role_from_id(member, WOW_ROLE_ID)
             elif reactionID == ROCKET_LEAGUE_EMOJI_ID:
                 print("Reacted Rocket League")
                 await add_role_from_id(member, ROCKET_LEAGUE_ROLE_ID)
             elif reactionID == HEARTHSTONE_EMOJI_ID:
                 print("Reacted Hearthstone")
                 await add_role_from_id(member, HEARTHSTONE_ROLE_ID)
-            elif reactionID == DESTINY_EMEOJI_ID:
-                print("Reacted Destiny")
-                await add_role_from_id(member, DESTINY_ROLE_ID)
             elif reactionID == RAINBOW_SIX_EMOJI_ID:
                 print("Reacted Rainbow Six Siege")
                 await add_role_from_id(member, RAINBOW_SIX_ROLE_ID)
             elif reactionID == PUBG_EMOJI_ID:
                 print("Reacted PUBG")
                 await add_role_from_id(member, PUBG_ROLE_ID)
-            elif reactionID == MTG_EMOJI_ID:
-                print("Reacted MTG")
-                await add_role_from_id(member, MTG_ROLE_ID)
-            elif reactionID == WARFRAME_EMOJI_ID:
-                print("Reacted Warframe")
-                await add_role_from_id(member, WARFRAME_ROLE_ID)
+            elif reactionID == FORTNITE_EMOJI_ID:
+                print("Reacted Fortnite")
+                await add_role_from_id(member, FORTNITE_ROLE_ID)
+            elif reactionID == DESTINY_EMEOJI_ID:
+                print("Reacted Destiny")
+                await add_role_from_id(member, DESTINY_ROLE_ID)
             else:
                 # Remove the reaction that is not relevant
                 channel = client.get_channel(payload.channel_id)
